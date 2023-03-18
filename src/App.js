8c1b0488ef70e8037bfaa9306c01606e66d3af7e
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-03-19 01:25:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+// import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddContact from "./components/AddContact";
+import UpdateContact from "./components/UpdateContact";
+import { useDispatch } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const data = [];
+        const fetchContact = async () => {
+            await fetch('https://jsonplaceholder.typicode.com/users')
+                .then((response) => response.json())
+                .then((json) => {
+                    json.map((contact) => {
+                      return(
+                        data.push({
+                          id: contact.id,
+                          name: contact.name,
+                          companyName: contact.company.name,
+                          userName: contact.username,
+                          number: contact.phone
+                       })
+                      )
+                        
+                    })
+                });
+            dispatch({ type: 'FETCH_CONTACTS', payload: data });
+        };
+        fetchContact();
+    });
+    return (
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route path="/Contact_List_App" element={<Home />}>
+
+                </Route>
+                <Route path="/add" element={<AddContact />}>
+
+                </Route>
+                <Route path="/edit/:id" element={<UpdateContact />}>
+
+                </Route>
+            </Routes>
+            <ToastContainer/>
+            
+        </div>
+    );
 }
 
 export default App;
